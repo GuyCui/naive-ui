8f4ad0f156eb0dda0eca614ed45e27aff8e5c0b8
GuyCui <18024642@qq.com>
2021-08-22 12:13:06 +0800

    add 对话框

--- src/main/java/com/example/naiveui/Application.java
+++ src/main/java/com/example/naiveui/Application.java
@@ -4,6 +4,8 @@ import com.example.naiveui.view.chat.ChatController;
 import com.example.naiveui.view.chat.IChatMethod;
 import javafx.stage.Stage;
 
+import java.util.Date;
+
 /**
  *@author GuyCui
  * @date 2021/8/20
@@ -20,12 +22,30 @@ public class Application extends javafx.application.Application {
 
     public static void main(String[] args) {
         launch(args);
-    }*/
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         IChatMethod chat = new ChatController();
         chat.doShow();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+*/
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        IChatMethod chat = new ChatController();
+        chat.doShow();
+
+        // 模拟测试
+        chat.addTalkBox(-1, 0, "1000001", "小傅哥", "01_50", "我不是一个简单的男人", new Date(), true);
+        chat.addTalkBox(-1, 0, "1000002", "铁锤", "02_50", "有本事现时里扎一下", new Date(), false);
+        chat.addTalkBox(-1, 0, "1000003", "团团", "03_50", "秋风扫过树叶落，哪有棋盘哪有我", new Date(), false);
+        chat.addTalkBox(-1, 0, "1000004", "哈尼克兔", "04_50", "你把爱放在我的心里", new Date(), false);
+        chat.addTalkBox(0, 1, "5307397", "虫洞 · 技术栈(1区)", "group_1", "小傅哥：吉祥健康、如意安康", new Date(), false);
     }
 
     public static void main(String[] args) {
