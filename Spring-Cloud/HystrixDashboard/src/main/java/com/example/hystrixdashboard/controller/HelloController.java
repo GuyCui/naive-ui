b44a4dee803750dc41c84a59d371798dbb374421
GuyCui <18024642@qq.com>
2021-12-03 20:03:15 +0800

    Hystrix容错处理controller结构调整
    Signed-off-by: GuyCui <18024642@qq.com>

--- Spring-Cloud/HystrixDashboard/src/main/java/com/example/hystrixdashboard/controller/HelloController.java
+++ Spring-Cloud/HystrixDashboard/src/main/java/com/example/hystrixdashboard/controller/HelloController.java
@@ -2,14 +2,15 @@ package com.example.hystrixdashboard.controller;
 
 
 import com.example.hystrixdashboard.HelloProducer;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 @RestController
 public class HelloController {
 
-    @Autowired
+    @Resource
     HelloProducer helloProducer;
 
     @RequestMapping("/hello")
