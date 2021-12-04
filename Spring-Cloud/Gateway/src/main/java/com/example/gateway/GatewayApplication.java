3eb176e4d852cc28fd1741202a003bcb9a332721
GuyCui <18024642@qq.com>
2021-12-04 22:35:34 +0800

    Gateway 网关服务注册到服务中心，实现服务转发

--- Spring-Cloud/Gateway/src/main/java/com/example/gateway/GatewayApplication.java
+++ Spring-Cloud/Gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -2,9 +2,6 @@ package com.example.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayApplication {
@@ -13,9 +10,9 @@ public class GatewayApplication {
         SpringApplication.run(GatewayApplication.class, args);
     }
 
-    @Bean
+    /*@Bean
     public RouteLocator routeLocator(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("hello2", r -> r.path("/hello2/**").uri("http://www.phei.com.cn")).build();
-    }
+    }*/
 }
