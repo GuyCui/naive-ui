3309b2a30d764a32c07ef9c85214e57eeba46359
GuyCui <18024642@qq.com>
2021-12-05 22:00:18 +0800

    Gateway端点添加动态路由

---
+++ Spring-Cloud/GatewayActuator/src/main/java/com/example/gatewayactuator/GatewayActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.gatewayactuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayActuatorApplication.class, args);
+    }
+
+}
