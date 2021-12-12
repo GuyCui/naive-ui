78e411458d5b102e97adbf259b3346c2cecee188
GuyCui <18024642@qq.com>
2021-12-12 21:28:07 +0800

    创建数据层

--- microservices-practical/social-multiplication/src/main/java/com/example/socialmultiplication/SocialMultiplicationApplication.java
+++ microservices-practical/social-multiplication/src/main/java/com/example/socialmultiplication/SocialMultiplicationApplication.java
@@ -3,11 +3,16 @@ package com.example.socialmultiplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 社交乘法应用
+ *
+ * @author Guy Cui
+ * @date 2021/12/08
+ */
 @SpringBootApplication
 public class SocialMultiplicationApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SocialMultiplicationApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(SocialMultiplicationApplication.class, args);
+  }
 }
