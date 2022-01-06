7b13761f65d5e6982e3e449b4e6983852134acc5
GuyCui <18024642@qq.com>
2022-01-06 21:20:09 +0800

    服务发现创建

---
+++ microservices-practical/serice-registry/src/main/java/com/example/sericeregistry/ServiceRegistryApplication.java
@@ -0,0 +1,21 @@
+package com.example.sericeregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 服务注册中心应用
+ *
+ * @author Guy Cui
+ * @date 2022/01/06
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class ServiceRegistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceRegistryApplication.class, args);
+    }
+
+}
