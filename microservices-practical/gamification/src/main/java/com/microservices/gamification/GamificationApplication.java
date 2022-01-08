a0f0a3967f5883e408814be47ed74d96076fb177
GuyCui <18024642@qq.com>
2022-01-08 19:58:35 +0800

    微服务网关 架构调整

--- microservices-practical/gamification/src/main/java/com/microservices/gamification/GamificationApplication.java
+++ microservices-practical/gamification/src/main/java/com/microservices/gamification/GamificationApplication.java
@@ -2,8 +2,16 @@ package com.microservices.gamification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * 游戏化应用程序
+ *
+ * @author Guy Cui
+ * @date 2022/01/08
+ */
 @SpringBootApplication
+@EnableEurekaClient
 public class GamificationApplication {
 
     public static void main(String[] args) {
