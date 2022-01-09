cd9ca6926c64589bf6ecd2604e8abb27082e0e70
GuyCui <18024642@qq.com>
2022-01-09 14:14:44 +0800

    乘法服务添加端口号日志

--- microservices-practical/social-multiplication/src/main/java/com/example/socialmultiplication/controller/MultiplicationController.java
+++ microservices-practical/social-multiplication/src/main/java/com/example/socialmultiplication/controller/MultiplicationController.java
@@ -2,6 +2,7 @@ package com.example.socialmultiplication.controller;
 
 import com.example.socialmultiplication.doMain.Multiplication;
 import com.example.socialmultiplication.service.MultiplicationService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,17 +15,21 @@ import org.springframework.web.bind.annotation.RestController;
  * @date: 2021/12/10
  */
 @RestController
+@Slf4j
 @RequestMapping("/multiplications")
 public class MultiplicationController {
-  private final MultiplicationService multiplicationService;
+    private final MultiplicationService multiplicationService;
+    private final int serverPort;
 
-  @Autowired(required = false)
-  public MultiplicationController(final MultiplicationService multiplication) {
-    this.multiplicationService = multiplication;
-  }
+    @Autowired(required = false)
+    public MultiplicationController(final MultiplicationService multiplication, int serverPort) {
+        this.multiplicationService = multiplication;
+        this.serverPort = serverPort;
+    }
 
-  @GetMapping("/random")
-  Multiplication getRandomMultiplication() {
-    return multiplicationService.createRandomMultiplication();
-  }
+    @GetMapping("/random")
+    Multiplication getRandomMultiplication() {
+        log.info("Generating a random multiplication from server @ {}", serverPort);
+        return multiplicationService.createRandomMultiplication();
+    }
 }
